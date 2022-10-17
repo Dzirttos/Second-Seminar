83e1dda985096b0227e4bcb48e9412474c1203a7
Dizrtt <demonic2003@bk.ru>
2022-10-18 00:19:22 +0200

    All homework tasks finished

---
+++ HW_2ndseminar/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+
+Console.WriteLine("Enter your number, please: ");
+int number = Convert.ToInt32(Console.ReadLine()); // Вариант, когда число вводится самостоятельно
+// int number = new Random().Next(1, 1000); //Варинат для случайных чисел
+
+Console.WriteLine($"The second digit of {number} is {number/10%10}");
+
+
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// Console.Clear();
+
+// Console.WriteLine("Enter your number, please: ");
+// int number = Convert.ToInt32(Console.ReadLine()); 
+
+// if (number<100)
+// {
+//     Console.WriteLine($"There is no third digit in {number}.");
+// }
+// else
+// {
+//     Console.WriteLine($"The third digit of {number} is {number.ToString()[2]}.");
+// }
+
+
+
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// Console.Clear();
+
+// Console.WriteLine("Enter day of the week using numbers, please: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if (number < 6)
+// {
+//     Console.WriteLine("Sadly it's not a weekend.");
+// }
+// else if (number >= 8)
+// {
+//     Console.WriteLine($"Ooops! There are only 7 days in a week, not {number}!");
+// }
+// else
+// {
+//     Console.WriteLine("Yay! It's a weekend!!!");
+// }
