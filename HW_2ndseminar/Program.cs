d5dca0290514b082b9fb21bf36835ff38e41f75b
Dizrtt <demonic2003@bk.ru>
2022-10-18 20:01:17 +0200

    Alternative way of solving task 13 added

--- HW_2ndseminar/Program.cs
+++ HW_2ndseminar/Program.cs
@@ -1,19 +1,25 @@
 ﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-Console.Clear();
+// Console.Clear();
 
-Console.WriteLine("Enter your number, please: ");
-int number = Convert.ToInt32(Console.ReadLine()); // Вариант, когда число вводится самостоятельно
-// int number = new Random().Next(1, 1000); //Варинат для случайных чисел
+// Console.WriteLine("Enter your number, please: ");
+// int number = Convert.ToInt32(Console.ReadLine()); // Вариант, когда число вводится самостоятельно
+// // int number = new Random().Next(1, 1000); //Варинат для случайных чисел
 
-Console.WriteLine($"The second digit of {number} is {number/10%10}");
+// Console.WriteLine($"The second digit of {number} is {number/10%10}");
 
 
 
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-// Console.Clear();
+Console.Clear();
 
-// Console.WriteLine("Enter your number, please: ");
-// int number = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Enter your number, please: ");
+int number = Convert.ToInt32(Console.ReadLine()); 
+int shownumber = number;
+while (number>1000)
+{
+    number = number/10;
+}
+Console.WriteLine($"The third digit of {shownumber} is {number%10}.");
 
 // if (number<100)
 // {
@@ -24,6 +30,8 @@ Console.WriteLine($"The second digit of {number} is {number/10%10}");
 //     Console.WriteLine($"The third digit of {number} is {number.ToString()[2]}.");
 // }
 
+// или просто одной стройкой: 
+//Console.WriteLine($"The third digit of {number} is {number.ToString()[2]}.");
 
 
 //Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
